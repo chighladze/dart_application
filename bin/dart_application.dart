720c3a4313ff928bff9e476fd41db006d4d68635
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-01-15 23:27:22 +0400

    dart hello world

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,5 +1,4 @@
-import 'package:dart_application/dart_application.dart' as dart_application;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application.calculate()}!');
+void main() {
+  print('Hello');
+  print('World!');
 }
