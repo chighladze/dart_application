d308aa24ea2703e2c41e0269c4a615d6e53affc6
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-01-16 21:36:52 +0400

    cycles

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -12,11 +12,34 @@ void main() {
   print(nums);
 
   List<int> nums2;
-  nums2 = [4, 6, 7];
+  nums2 = [4, 6, 7, 5, 7, 8];
   print('First: ${nums[0]}. Last: ${nums.last}. Length: ${nums.length}');
 
   // Set<int> digits;
   var digits = {5, 6, 7, 2, 5, 3, 6};
 
   print(digits);
+
+  for (var el in nums2) {
+    el++;
+    print('Element: ${el}');
+  }
+  print('---------------------');
+
+  nums2.forEach((el) {
+    el++;
+    print(el);
+  });
+
+  var i = 1;
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+  print('---------------------');
+  var i2 = 100;
+  do {
+    print(i2);
+    i2++;
+  } while (i2 < 10);
 }
