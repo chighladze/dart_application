d575b9a48168fb5c00fe69291aff7e00877b7a4e
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-01-16 20:21:00 +0400

    variables and data types

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,4 +1,24 @@
+const int = 10;
+var word = 'Hello';
+
 void main() {
   print('Hello');
   print('World!');
+
+  var point = 5.5;
+  String some = 'Hello';
+  bool isHappy;
+  isHappy = true;
+
+  print('variable: ${point + 0.5}. And ${some.toUpperCase()}');
+
+  dynamic dynamicData = 5;
+  dynamicData = 'world';
+  var a = 15, b = 5;
+  print("- :${a - b}");
+  print("+ :${a + b}");
+  print("* :${a * b}");
+  print("/ :${(a / 5).toInt()}");
+  print("/ :${a ~/ 5}");
+  print("% :${a % b}");
 }
