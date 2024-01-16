e83a4c5dca617687140886ef414586ccb80598d0
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-01-16 21:27:30 +0400

    lsit & set

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,14 +1,22 @@
 void main() {
-  int a = 5;
-  int b = 5;
+  var nums = [5, 7, 8, 'hello', true]; // List()
+  nums.add(7);
+  print(nums);
+  nums.addAll([5, 7, 3, 1]);
+  print(nums);
+  nums.remove(8);
+  print(nums);
+  nums.removeAt(0);
+  print(nums);
+  // nums.removeWhere((element) => element >= 5);
+  print(nums);
 
-  if (a == b && b > 5) {
-    print('hello');
-  } else if (a > b || a == b) {
-    print("a > b");
-  } else if (a < b) {
-    print("a < b");
-  } else {
-    print("else");
-  }
+  List<int> nums2;
+  nums2 = [4, 6, 7];
+  print('First: ${nums[0]}. Last: ${nums.last}. Length: ${nums.length}');
+
+  // Set<int> digits;
+  var digits = {5, 6, 7, 2, 5, 3, 6};
+
+  print(digits);
 }
